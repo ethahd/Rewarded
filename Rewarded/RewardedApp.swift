b5da4deeb2773154821b8af76ffeedb86c0b09fa
ethahd <ahd@unc.edu>
2025-04-21 14:32:00 -0400

    Initial Commit

---
+++ Rewarded/RewardedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewardedApp.swift
+//  Rewarded
+//
+//  Created by Ethan AK on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct RewardedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RewardedTests/RewardedTests.swift
@@ -0,0 +1,16 @@
+//
+//  RewardedTests.swift
+//  RewardedTests
+//
+//  Created by Ethan AK on 4/21/25.
+//
+
+import Testing
+
+struct RewardedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
